009bfebfedd4b3cd4d3ff82c9f818595e8e23336
Ashish kumar upadhyay <ashishkumarupadhyay0328@gmail.com>
2026-05-14 13:46:12 +0530

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import RefreshRedirect from '@/components/RefreshRedirect'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Ashish Kumar Upadhyay",
   description: "Portfolio...",
+  verification: {
+    google: "AcvwynYb7Zgxrq3yFTFIeyRxnwOxwU7a_AARIseYezw",
+  },
 };
 
 export default function RootLayout({
